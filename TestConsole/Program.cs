df5df751860f1951f152534cb650a2d899f3d121
fantasticswallow <f_swallow@live.com>
2013-11-02 23:29:10 +0900

    TestConsoleコミットし忘れ

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -21,12 +21,38 @@ namespace TestConsole
             var test5 = "_ => _.hoge() && !(_.fuga() || _.piyo)";
             var test6 = "From(\"hoge\").Where(_ => _ + 10 > (_ - 10) && x - y< _ - 10 || ((x + 5) == (y != 5) && _ != 5) ).Select(_ => _.Replace(\"x1\",\"x2\"))";
             var test7 = "Skip(15)";
+            var test8 = "Where(_ => _ % 5 == 0).Select(_ => _.ToString() + \"hoge\")";
+            var test9 = "Where(_ => _ % 5 == 0).Select(_ => _ + 2).Select(_ => _.ToString() + \"hoge\")";
+            var test10 = "From(\"Range1\").Where(_ => _ % 5 == 0).Select(_ => _ + 2).Select(_ => _.ToString() + \"hoge\")";
+            var test11 = "From(\"Range2\").Where(_ => _ % 5 == 0).Select(_ => _ + 2).Select(_ => _.ToString() + \"hoge\")";
+            var test12 = "From(\"TestData1\").Where(_ => _.Lv > 45).OrderByDescending(_ => _.Lv).Take(5)";
+            var test13 = "From(\"TestData1\").OrderByDescending(_ => _.Lv).GroupBy(_ => _.ShipType)";
             //var list = TestBinder.MethodParseTest(test);
             //list.ForEach(_ => Console.WriteLine(_));
-            Console.WriteLine(test7);
+            var curLinear = new Linear();
+            curLinear.MemoriedCollection.Add("Range1", Enumerable.Range(12, 100000));
+            curLinear.MemoriedCollection.Add("Range2", Enumerable.Range(110, 15));
+            curLinear.MemoriedCollection.Add("TestData1", testData1.GetData());
+            Console.WriteLine(test10);
             Console.WriteLine("");
-            var obj = TestBinder.LambdaExpressionDynamicTest(Enumerable.Range(1,100).AsQueryable<int>(),test7);
+            var obj = curLinear.GetResult<string>(test10);
             obj.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(test12);
+            Console.WriteLine("");
+            var obj2 = curLinear.GetResult<testData1>(test12);
+            obj2.ForEach(x => Console.WriteLine(string.Format("{0} : {1} : {2}",x.Name ,x.Lv.ToString(),x.ShipType)));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(test13);
+            Console.WriteLine("");
+            var obj3 = curLinear.GetResult<IGrouping<string,testData1>>(test13);
+            obj3.ForEach(x => {
+                Console.WriteLine(x.Key + " => ");
+                x.ForEach(x2 => Console.WriteLine(string.Format("{0} : {1} : {2}", x2.Name, x2.Lv.ToString(), x2.ShipType)));
+                Console.WriteLine("");
+            });
         }
 
         
---
+++ TestConsole/testData1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    internal class testData1
+    {
+        internal string Name { get; set; }
+        internal int Lv { get; set; }
+        internal string ShipType { get; set; }
+
+
+        internal static IEnumerable<testData1> GetData()
+        {
+            var l = new List<testData1>();
+            l.Add(testData1.Create("Fubuki", 65, "Destroyer"));
+            l.Add(testData1.Create("Haruna", 61, "BattleShip"));
+            l.Add(testData1.Create("Syouhou", 58, "Light Aircraft Carrer"));
+            l.Add(testData1.Create("Hiei", 59, "BattleShip"));
+            l.Add(testData1.Create("Shimakaze", 50, "Destroyer"));
+            l.Add(testData1.Create("Atago", 61, "Heavy Cruiser"));
+            l.Add(testData1.Create("Abukuma", 39, "Light Cruiser"));
+            l.Add(testData1.Create("Syokaku", 43, "Aircraft Carrer"));
+            l.Add(testData1.Create("I168", 55, "Submarine"));
+            l.Add(testData1.Create("Zuikaku", 52, "Aircraft Carrer"));
+            l.Add(testData1.Create("Ayanami", 63, "Destroyer"));
+            l.Add(testData1.Create("Zuihou", 51, "Light Aircraft Carrer"));
+            l.Add(testData1.Create("Yura", 42, "Light Cruiser"));
+            l.Add(testData1.Create("Ashigara", 38, "Heavy Cruiser"));
+            l.Add(testData1.Create("Kongou", 53, "BattleShip"));
+            return l;
+        }
+
+        internal static testData1 Create(string name,int lv,string st)
+        {
+            var obj = new testData1();
+            obj.Name = name;
+            obj.Lv = lv;
+            obj.ShipType = st;
+            return obj;
+        }
+
+    }
+
+    
+    
+}
